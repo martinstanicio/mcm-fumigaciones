a2aeb99432552663013bf9ea8fcea2e65a77c121
Martín Stanicio <martuytimo@gmail.com>
2026-02-24 18:05:53 +0000

    create navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Navbar } from "@/components/navbar";
 import { cn } from "@/lib/utils";
 import { Inter } from "next/font/google";
 
@@ -11,7 +12,10 @@ type Props = Readonly<{ children: React.ReactNode }>;
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="es-AR">
-      <body className={cn(inter.variable, "antialiased")}>{children}</body>
+      <body className={cn(inter.variable, "antialiased")}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/content/navigation.ts
@@ -0,0 +1,19 @@
+type NavigationItem = {
+  title: string;
+  href: string;
+};
+
+export const navigation: NavigationItem[] = [
+  {
+    title: "Inicio",
+    href: "/",
+  },
+  {
+    title: "Servicios",
+    href: "/servicios",
+  },
+  {
+    title: "Contacto",
+    href: "/contacto",
+  },
+];
