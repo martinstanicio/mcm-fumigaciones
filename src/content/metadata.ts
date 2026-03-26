96852377ac1f1b25b13596dde4b42d388cdaf532
Martín Stanicio <martuytimo@gmail.com>
2026-03-26 17:42:09 +0000

    update meta description and keywords

--- src/content/metadata.ts
+++ src/content/metadata.ts
@@ -4,18 +4,36 @@ import type { Metadata, Viewport } from "next";
 export const siteName = "MCM Fumigaciones";
 export const title = "Protegemos tu hogar y negocio de plagas";
 export const description =
-  "Especialistas en control de plagas para comercios, oficinas, fábricas, viviendas, barrios privados, clubes de campo y más.";
+  "Especialistas en fumigación y control de plagas en Escobar. Eliminamos cucarachas, roedores, hormigas y más en hogares y comercios.";
 export const keywords = [
   siteName,
   ...plagues,
-  "control de plagas",
-  "fumigaciones",
-  "eliminación de plagas",
   "servicios de fumigación",
-  "fumigación de comercios",
-  "fumigación de hogares",
-  "escobar",
-  "buenos aires",
+  "fumigaciones",
+  "fumigación",
+  "fumigación comercios",
+  "fumigación hogares",
+  "fumigación Escobar",
+  "fumigación Garín",
+  "fumigación Maschwitz",
+  "fumigación Maquinista Savio",
+  "fumigación Matheu",
+  "fumigación Zelaya",
+  "fumigación Zona Norte",
+  "fumigación Buenos Aires",
+  "fumigación cucarachas",
+  "fumigación mosquitos",
+  "fumigación pulgas",
+  "fumigación urgencia",
+  "control plagas",
+  "control plagas residencial",
+  "control escorpiones",
+  "control roedores",
+  "desratización",
+  "eliminar hormigas",
+  "eliminar plagas",
+  "exterminar chinches de cama",
+  "presupuesto de fumigación",
 ];
 export const url = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? new URL(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`)
